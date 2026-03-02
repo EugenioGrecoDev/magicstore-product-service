e232b5b5513d2361b27c7f6ffd1c97071d7e3505
Eugenio Greco <euge.g91@gmail.com>
2026-03-02 23:54:52 +0100

    Initial commit

---
+++ src/main/java/com/eugeniogreco/magicstore/MagicstoreApplication.java
@@ -0,0 +1,13 @@
+package com.eugeniogreco.magicstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicstoreApplication.class, args);
+	}
+
+}
