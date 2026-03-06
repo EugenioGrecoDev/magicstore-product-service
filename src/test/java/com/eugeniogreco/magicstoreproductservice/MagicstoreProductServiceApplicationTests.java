847fdb1a5929f63dc567077bcda2e80173d2b541
Eugenio Greco <euge.g91@gmail.com>
2026-03-06 22:19:40 +0100

    add readme, securityconfig and minor fixes

--- src/test/java/com/eugeniogreco/magicstoreproductservice/MagicstoreProductServiceApplicationTests.java
+++ src/test/java/com/eugeniogreco/magicstoreproductservice/MagicstoreProductServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.eugeniogreco.magicstore;
+package com.eugeniogreco.magicstoreproductservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MagicstoreApplicationTests {
+class MagicstoreProductServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
